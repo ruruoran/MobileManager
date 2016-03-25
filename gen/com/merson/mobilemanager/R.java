a3d162a84066376ff7cc30d1f537a18d2382bf11
ruruoran <ruruoran@126.com>
2016-03-26 00:26:00 +0800

    完成Home页面的布局

--- gen/com/merson/mobilemanager/R.java
+++ gen/com/merson/mobilemanager/R.java
@@ -1068,8 +1068,18 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
-        public static final int logo2=0x7f020058;
+        public static final int app=0x7f020057;
+        public static final int atools=0x7f020058;
+        public static final int callmsgsafe=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
+        public static final int logo2=0x7f02005b;
+        public static final int main=0x7f02005c;
+        public static final int netmanager=0x7f02005d;
+        public static final int safe=0x7f02005e;
+        public static final int settings=0x7f02005f;
+        public static final int sysoptimize=0x7f020060;
+        public static final int taskmanager=0x7f020061;
+        public static final int trojan=0x7f020062;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1096,18 +1106,20 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int gv_home_content=0x7f05003d;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int iv_gv_icon=0x7f050040;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int pb_splash_down=0x7f05003d;
+        public static final int pb_splash_down=0x7f05003f;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1130,7 +1142,9 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int tv_splash_version=0x7f05003c;
+        public static final int tv_gv_name=0x7f050041;
+        public static final int tv_home_welcome=0x7f05003c;
+        public static final int tv_splash_version=0x7f05003e;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1187,7 +1201,8 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_home=0x7f030018;
         public static final int activity_splash=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int item_gridview=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
--- src/com/merson/mobile/mobilemanager/HomeActivity.java
+++ src/com/merson/mobile/mobilemanager/HomeActivity.java
@@ -6,12 +6,142 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
+
+	private TextView tv_home_welcome;
+	private GridView gv_home_content;
+	
+	private final int CONTENT_NUM = 9;
+	private String[] titles = {"手机防盗","通讯卫士","软件管理",
+			 "进程管理","流量统计","手机杀毒",
+			 "缓存清理","高级工具","设置中心"};
+	
+	//盛装照片
+	private int[] iconarray = {R.drawable.safe,R.drawable.callmsgsafe,R.drawable.app
+			 ,    R.drawable.taskmanager,R.drawable.netmanager,R.drawable.trojan,
+			    R.drawable.sysoptimize,R.drawable.atools,R.drawable.settings};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		 tv_home_welcome = (TextView) findViewById(R.id.tv_home_welcome);
+		 gv_home_content = (GridView) findViewById(R.id.gv_home_content);
+		 
+		 
+		 tv_home_welcome.setText("欢迎您,新用户,我们的应用可以保卫您手机的安全！");
+		//方法2,让控件处于选中状态
+		//tv_home_welcome.setSelected(true);
+		 
+		 //初始化九宫格
+		 //相似与ListView
+		 gv_home_content.setAdapter(new MyAdapter());
+		 
+		 gv_home_content.setOnItemClickListener(new MyItemOnClickListener());
+		 
 	}
+	
+	
+	class MyItemOnClickListener implements AdapterView.OnItemClickListener{
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			// TODO Auto-generated method stub
+			switch (position) {
+			case 0:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 1:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 2:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 3:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 4:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 5:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 6:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 7:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+				
+			case 8:
+				Toast.makeText(HomeActivity.this,titles[position],Toast.LENGTH_LONG).show();
+				break;
+
+			default:
+				break;
+			}
+			
+		}
+		
+	}
+	
+	
+	class MyAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return CONTENT_NUM;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View converView, ViewGroup parent) {
+			//GridView 优化  可参考Listview 优化
+			// 方法1 ：复用convertview
+			// 方法2 ：viewholder
+			// TODO Auto-generated method stub
+			
+			View v = View.inflate(HomeActivity.this, R.layout.item_gridview, null);
+			//注意： v.findViewById  否则是本activity（home）下的
+			ImageView iv_gv_icon = (ImageView) v.findViewById(R.id.iv_gv_icon);
+			TextView tv_gv_name = (TextView) v.findViewById(R.id.tv_gv_name);
+			
+			iv_gv_icon.setImageResource(iconarray[position]);
+			tv_gv_name.setText(titles[position]);
+			return v;
+		}
+		
+	}
+	
+	
 }
